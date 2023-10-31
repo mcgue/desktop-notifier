fcdf132c56da86281f1b7a6009708c439a7ac98d
MC <mc8@protonmail.com>
2023-10-31 11:25:41 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# Sends notification to desktop
+
+# get modules
+import requests
+import plyer
+
+#
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+#
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+
